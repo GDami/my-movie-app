ce6d21180349f03ac973469871f2af8c60681761
Damien <d.gervy@free.fr>
2025-09-10 12:44:53 +0200

    added a logo and a homepage

--- react-app/src/pages/Home/Home.tsx
+++ react-app/src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import SearchBar from "../../components/SearchBar/SearchBar"
 
 export default function Home() {
     return (
-        <div className="home bg-white flex flex-col gap-10 items-center justify-center lg:justify-start lg:pt-20 min-h-screen max-w-[1280px] mx-auto p-4">
+        <div className="home flex flex-col gap-10 items-center justify-center lg:justify-start lg:pt-20 min-h-screen max-w-[1280px] mx-auto p-4">
             <img className="w-[600px]" src={logo} alt="logo"></img>
             <SearchBar />
         </div>
