290d3ed9ea5b5c14971cb5d8845e5bc986ece8c7
Damien <d.gervy@free.fr>
2025-09-12 15:44:07 +0200

    white logo on the home page

--- react-app/src/pages/Home/Home.tsx
+++ react-app/src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
-import logo from "/logo-mouvise.png"
+import logo from "/logo-mouvise-white-transparent.png"
+import tmdbLogo from "/tmdb-white.svg"
 import SearchBar from "../../components/SearchBar/SearchBar"
 
 export default function Home() {
     return (
         <div className="home flex flex-col gap-10 items-center justify-center lg:justify-start lg:pt-20 min-h-screen max-w-[1280px] mx-auto p-4">
-            <img className="w-[600px]" src={logo} alt="logo"></img>
+            <div className="relative w-[600px]">
+                <img className="" src={logo} alt="logo"></img>
+                <img className="absolute w-1/3 bottom-0 right-0" src={tmdbLogo} alt="tmdb-logo"></img>
+            </div>
             <SearchBar />
         </div>
     )
