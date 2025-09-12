7aaa861ba992e7828f2ada0a20b8359def1e43cb
Damien <d.gervy@free.fr>
2025-09-12 15:43:36 +0200

    changement du style des tiles, ajout d'un padding autour de la liste des résultats

--- react-app/src/pages/SearchResults/SearchResult.tsx
+++ react-app/src/pages/SearchResults/SearchResult.tsx
@@ -6,9 +6,9 @@ type SearchResultProps = {
 
 export default function SearchResult(props: SearchResultProps) {
     return (
-        <div className="hover:bg-white h-40 flex gap-3 rounded-20 shadow-sm overflow-clip hover:shadow-lg hover:cursor-pointer transition-shadow">
-            <img className="border border-black rounded-20 h-full aspect-23/40 flex justify-center items-center" src={props.imageUrl ? props.imageUrl : undefined} alt={"NO IMG"}></img>
-            <div className="flex flex-col gap-2 py-3 pr-3">
+        <div className="hover:bg-white h-40 flex gap-3 p-3 shadow-sm overflow-clip hover:shadow-lg hover:cursor-pointer transition-shadow">
+            <img className="h-full aspect-23/40 flex justify-center items-center" src={props.imageUrl ? props.imageUrl : undefined} alt={"no img"}></img>
+            <div className="flex flex-col gap-2">
                 <p>{props.title}</p>
                 <p className="text-gray-600 overflow-hidden shadow-[inset_0_30px_40px_rgba(255,255,255,.1)]">{props.description}</p>
             </div>
