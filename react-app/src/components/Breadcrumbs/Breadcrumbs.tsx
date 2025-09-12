62192f9a57dba4ba8d65b8e2f5285bbca6622567
Damien <d.gervy@free.fr>
2025-09-12 10:12:14 +0200

    added mouvise to the projects, removed mods and random-spotify

--- react-app/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ react-app/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
 
 export default function Breadcrumbs() {
+    
+
     return (
         <div className="breadcrumbs w-fit text-sm text-gray-600 flex py-2 ">
             <Link to="/" className="breadcrumb hover:underline">Home</Link>&nbsp;&gt;&nbsp; 
