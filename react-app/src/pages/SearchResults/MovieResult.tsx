a91b6aa86ef7cc46401e68d86aeffee21353c0c0
Damien <d.gervy@free.fr>
2025-09-13 16:17:42 +0200

    fixed image sizes and text-ellipses on person results not working

--- react-app/src/pages/SearchResults/MovieResult.tsx
+++ react-app/src/pages/SearchResults/MovieResult.tsx
@@ -6,8 +6,8 @@ type MovieResultProps = {
 
 export default function MovieResult(props: MovieResultProps) {
     return (
-        <div className="border border-gray-300 hover:bg-white h-40 select-none flex shadow-md shadow-black/20 overflow-clip hover:-translate-0.5 hover:shadow-lg hover:shadow-mouvise/70 hover:border-mouvise hover:text-mouvise hover:cursor-pointer transition-all">
-            <img className="h-full aspect-23/40 flex justify-center items-center" src={props.imageUrl ? props.imageUrl : undefined} alt={"no img"}></img>
+        <div className="box-content border border-gray-300 hover:bg-white h-40 select-none flex shadow-md shadow-black/20 overflow-clip hover:-translate-0.5 hover:shadow-lg hover:shadow-mouvise/70 hover:border-mouvise hover:text-mouvise hover:cursor-pointer transition-all">
+            <img className="shrink-0 h-full aspect-23/40 flex justify-center items-center" src={props.imageUrl ? props.imageUrl : undefined} alt={"no img"}></img>
             <div className="flex flex-col p-3 gap-1 mask-b-from-75%">
                 <p className="font-bold">{props.title}</p>
                 <p className={`relative text-gray-600 overflow-hidden ${props.description ? "" : "italic"}`}>{props.description ? props.description : "no summary"}</p>
--- react-app/src/pages/SearchResults/PersonResult.tsx
+++ react-app/src/pages/SearchResults/PersonResult.tsx
@@ -7,11 +7,11 @@ type PersonResultProps = {
 
 export default function PersonResult(props: PersonResultProps) {
     return (
-        <div className="rounded-20 border border-gray-300 hover:bg-white h-23 select-none flex shadow-md shadow-black/20 overflow-clip hover:-translate-0.5 hover:shadow-lg hover:shadow-mouvise/50 hover:border-mouvise hover:text-mouvise hover:cursor-pointer transition-all">
+        <div className="box-content rounded-20 border border-gray-300 hover:bg-white h-23 select-none flex shadow-md shadow-black/20 overflow-clip hover:-translate-0.5 hover:shadow-lg hover:shadow-mouvise/50 hover:border-mouvise hover:text-mouvise hover:cursor-pointer transition-all">
             <img className="shrink-0 rounded-l-20 w-23 h-full object-cover flex justify-center items-center" src={props.imageUrl ? props.imageUrl : undefined} alt={"no img"}></img>
-            <div className="flex flex-col justify-center p-3 gap-1 mask-b-from-80%">
+            <div className="flex flex-col justify-center p-3 gap-1 w-[calc(100%-92px)]">
                 <div><span className="font-bold">{props.name}</span> - <span>{props.knownForDepartment}</span></div>
-                <p className="relative text-gray-600 truncate">{props.knownFor.join(", ")}</p>
+                <p className="relative text-gray-600 truncate w-[calc(100%)]">{props.knownFor.join(", ")}</p>
             </div>
         </div>
     )
