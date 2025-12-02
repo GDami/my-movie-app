89366d55210bdd559eb66dece63782459de803f4
GDami <41579255+GDami@users.noreply.github.com>
2025-12-02 18:34:45 +0100

    port in env

--- express-app/app.js
+++ express-app/app.js
@@ -1,11 +1,16 @@
 const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
 const app = express()
-const port = 3000
+
+const PORT = process.env.PORT
 
 app.get("/", (req, res) => {
     res.send("Hello World !")
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
